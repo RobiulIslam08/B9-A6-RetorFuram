905976bf45c7bbd479ee1db1e9b6ee6f68160060
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-03-03 12:55:47 +0600

    card design complete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const allPost = async() => {
         <div class="w-[15%] p-5">
           <div class="indicator">
             <span class="indicator-item badge badge-accent"></span>
-            <div class="grid h-[72px] w-[72px] bg-base-300 place-items-center">content</div>
+            <img src=${item.image} alt="" class="grid h-[72px] w-[72px] bg-base-300 place-items-center">
           </div>
         </div>
         <!-- details -->
@@ -21,15 +21,15 @@ const allPost = async() => {
 
           <!-- 1st -->
           <div class="text-sm font-inter font-medium text-[#12132DCC] flex gap-7 mb-[12px]">
-            <h1># <span >Music</span></h1>
-            <h1>Author: Awlad Hossain</h1>
+            <h1># <span >${item.category}</span></h1>
+            <h1>Author:${item.author.name}</h1>
           </div>
 
           <!-- 2nd heading-->
-          <h1 class="font-mulish text-xl font-bold text-[#12132D] mb-[17px]">10 Kids Unaware of Their Halloween Costume</h1>
+          <h1 class="font-mulish text-xl font-bold text-[#12132D] mb-[17px]">${item.title}</h1>
 
           <!-- 3rd -->
-          <p class="text-[#12132D99] text-base font-inter font-normal mb-[44px]">It’s one thing to subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
+          <p class="text-[#12132D99] text-base font-inter font-normal mb-[44px]">${item.description}</p>
 
         
 
@@ -38,19 +38,19 @@ const allPost = async() => {
            <div class="flex gap-8">
             <div class="flex gap-2">
               <img src="images/messege.png" alt="">
-              <p class="text-[#12132D99] text-base font-inter font-normal">550</p>
+              <p class="text-[#12132D99] text-base font-inter font-normal">${item.comment_count}</p>
             </div>
             <div  class="flex gap-2">
               <img src="images/eye.png" alt="">
-              <p class="text-[#12132D99] text-base font-inter font-normal">1566</p>
+              <p class="text-[#12132D99] text-base font-inter font-normal">${item.view_count}</p>
             </div>
             <div  class="flex gap-2">
               <img src="images/clock.png" alt="">
-              <p class="text-[#12132D99] text-base font-inter font-normal">5 min</p>
+              <p class="text-[#12132D99] text-base font-inter font-normal"><span>${item.posted_time}</span> min</p>
             </div>
            </div>
            <div>
-            <img src="images/email.png" alt="">
+            <button class="bg-[#10B981] px-2 rounded-full"><i class="fa-solid fa-envelope"></i></button>
            </div>
 
           </div>
